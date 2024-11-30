4b600482e1cc05b66efddacc1340b73f9d686874
Bram Mortier <79015250+BramMortier@users.noreply.github.com>
2024-11-30 19:10:43 +0100

    added rendering recipes from localstorage

--- javascript/components/RenderRecipe.js
+++ javascript/components/RenderRecipe.js
@@ -1,15 +1,31 @@
 class RenderRecipe extends HTMLElement {
 	constructor() {
 		super();
+
+		this.recipeData = JSON.parse(localStorage.getItem("recipe"));
 	}
 
 	connectedCallback() {
+		this.initElements();
 		this.renderGeneralInfo();
 		this.renderIngredients();
 		this.renderSteps();
 	}
 
-	renderGeneralInfo() {}
+	initElements() {
+		this.ingredientsList = this.querySelector(".b-ingredients-list");
+		console.log(this.ingredientsList);
+	}
+
+	renderGeneralInfo() {
+		this.recipeData.ingredients.forEach((ingredient) => {
+			const ingredientItem = document.createElement("li");
+			ingredientItem.classList.add("b-ingredients-list-item");
+			ingredientItem.innerText = ingredient;
+
+			this.ingredientsList.appendChild(ingredientItem);
+		});
+	}
 
 	renderIngredients() {}
 
